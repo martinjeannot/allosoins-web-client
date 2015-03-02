44b87c52f53c4120f3a8693a229c4117e091599e
Martin Jeannot <martin.jeannot@outlook.com>
2015-03-02 23:13:19 +0100

    Add agenda controller, view and route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,10 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/agenda', {
+        templateUrl: 'views/agenda.html',
+        controller: 'AgendaController'
+      })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
---
+++ app/scripts/controllers/agenda.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allosoinsWebClientApp.controller:AgendaController
+ * @description
+ * # AgendaController
+ * Controller of the Agenda feature
+ */
+angular.module('allosoinsWebClientApp')
+  .controller('AgendaController', function () {});
